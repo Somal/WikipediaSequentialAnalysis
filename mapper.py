3332589bde1a4a2aae12305aa732ba64b4d008f3
somal <somal1996@rambler.ru>
2017-03-19 16:00:57 +0300

    Update mapper

--- mapper.py
+++ mapper.py
@@ -29,28 +29,31 @@ EXCEPTED_TITLES = ['404_error/',
 for line in sys.stdin:
     line = line.strip()
     if line[:2].lower() == 'en':
-        project_name, page_title, number_of_access, total_data = line.split()
+        try:
+            project_name, page_title, number_of_access, total_data = line.split()
 
-        tags_including = False
-        for tag in EXCEPTED_TAGS:
-            if page_title.startswith(tag):
-                tags_including = True
-                break
+            tags_including = False
+            for tag in EXCEPTED_TAGS:
+                if page_title.startswith(tag):
+                    tags_including = True
+                    break
 
-        if not tags_including:
-            if not page_title.split(':')[-1][0].islower():
-                extension_including = False
-                for ext in EXCEPTED_EXTENSTIONS:
-                    if page_title.endswith(ext):
-                        extension_including = True
-                        break
-
-                if not extension_including:
-                    title_including = False
-                    for title in EXCEPTED_TITLES:
-                        if page_title == title:
-                            title_including = True
+            if not tags_including:
+                if not page_title.split(':')[-1][0].islower():
+                    extension_including = False
+                    for ext in EXCEPTED_EXTENSTIONS:
+                        if page_title.endswith(ext):
+                            extension_including = True
                             break
 
-                    if not title_including:
-                        print("{}/t{}".format(page_title, number_of_access))
+                    if not extension_including:
+                        title_including = False
+                        for title in EXCEPTED_TITLES:
+                            if page_title == title:
+                                title_including = True
+                                break
+
+                        if not title_including:
+                            print("{}\t{}".format(page_title, number_of_access))
+        except:
+            pass
