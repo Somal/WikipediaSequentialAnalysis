d2649711c7f0322be4d4f9bfb01617e8677bef69
somal <somal1996@rambler.ru>
2017-03-19 16:08:54 +0300

    Add reducer

---
+++ reducer.py
@@ -0,0 +1,20 @@
+import sys
+
+current_title = None
+current_count = 0
+for line in sys.stdin:
+    title, count = line.split('\t')
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if title == current_title:
+        current_count += count
+    else:
+        print("{}\t{}".format(current_title, current_count))
+        current_title, current_count = title, count
+
+if current_title == title:
+    print("{}\t{}".format(current_title, current_count))
