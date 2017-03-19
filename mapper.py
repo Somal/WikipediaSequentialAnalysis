4a29dd354607ae8d84f3342012f04db571ba583f
somal <somal1996@rambler.ru>
2017-03-19 15:57:48 +0300

    Add mapper

--- mapper.py
+++ mapper.py
@@ -1,1 +1,56 @@
+import sys
 
+EXCEPTED_TAGS = ['Media:',
+                 'Special:',
+                 'Talk:',
+                 'User:',
+                 'User_talk:',
+                 'Project:',
+                 'Project_talk:',
+                 'File:',
+                 'File_talk:',
+                 'MediaWiki:',
+                 'MediaWiki_talk:',
+                 'Template:',
+                 'Template_talk:',
+                 'Help:',
+                 'Help_talk:',
+                 'Category:',
+                 'Category_talk:',
+                 'Portal:',
+                 'Wikipedia:',
+                 'Wikipedia_talk:', ]
+
+EXCEPTED_EXTENSTIONS = ['.jpg', '.gif', '.png', '.JPG', '.GIF', '.PNG', '.txt', '.ico']
+EXCEPTED_TITLES = ['404_error/',
+                   'Main_Page',
+                   'Hypertext_Transfer_Protocol',
+                   'Search']
+for line in sys.stdin:
+    line = line.strip()
+    if line[:2].lower() == 'en':
+        project_name, page_title, number_of_access, total_data = line.split()
+
+        tags_including = False
+        for tag in EXCEPTED_TAGS:
+            if page_title.startswith(tag):
+                tags_including = True
+                break
+
+        if not tags_including:
+            if not page_title.split(':')[-1][0].islower():
+                extension_including = False
+                for ext in EXCEPTED_EXTENSTIONS:
+                    if page_title.endswith(ext):
+                        extension_including = True
+                        break
+
+                if not extension_including:
+                    title_including = False
+                    for title in EXCEPTED_TITLES:
+                        if page_title == title:
+                            title_including = True
+                            break
+
+                    if not title_including:
+                        print("{}/t{}".format(page_title, number_of_access))
