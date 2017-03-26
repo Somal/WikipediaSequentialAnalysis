e18ea40b220350bba7b8b1ef71af2113e0cdb0de
somal <somal1996@rambler.ru>
2017-03-26 12:41:47 +0300

    Update reducer

--- IA2/reducer.py
+++ IA2/reducer.py
@@ -1,5 +1,7 @@
 import sys
 
+THRESHOLD = 10000
+
 current_title = None
 count_by_date = {}
 for line in sys.stdin:
@@ -16,14 +18,14 @@ for line in sys.stdin:
     else:
         if current_title is not None:
             total_month_views = sum(count_by_date.values())
-            print(total_month_views)
-            if total_month_views > 100000:
-                print("{}\t{}".format(total_month_views, title) + "\t".join(
+            # print(total_month_views)
+            if total_month_views > THRESHOLD:
+                print("{}\t{}\t".format(total_month_views, title) + "\t".join(
                     ["{}:{}".format(date, count_by_date[date]) for date in sorted(count_by_date.keys())]))
         current_title = title
         count_by_date = {date: count}
 
 total_month_views = sum(count_by_date.values())
-if total_month_views > 100000:
+if total_month_views > THRESHOLD:
     print("{}\t{}".format(total_month_views, current_title) + "\t".join(
         ["{}:{}".format(date, count) for date, count in count_by_date.items()]))
